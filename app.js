8f4276634f5e53354dcbbd218b82fe7ed1a1552e
zplsh <splash792@gmail.com>
2018-06-03 16:10:12 +0300

    new changes! Added movie view

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 
-<<<<<<< HEAD
 'use strict';
 
 function httpGet(url) {
 
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
 
         let xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
 
-        xhr.onload = function() {
+        xhr.onload = function () {
             if (this.status == 200) {
                 resolve(this.response);
             } else {
@@ -19,71 +18,35 @@ function httpGet(url) {
             }
         };
 
-        xhr.onerror = function() {
+        xhr.onerror = function () {
             reject(new Error("Network Error"));
         };
 
         xhr.send();
     });
-
-=======
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.omdbapi.com/?apikey=adac724d&t=batman", true);
-//xhr.withCredentials = true;
-xhr.onload = function Test() {
-    let film  = xhr.responseText;
-    let result =   document.getElementById("result");
-    result.innerHTML = film;
->>>>>>> 2686c5448a16b3678da9672683327142aba413e5
-};
-
-
+}
 httpGet("http://www.omdbapi.com/?apikey=adac724d&t=batman")
     .then(response => {
+        console.log(response);
         let filmObj = JSON.parse(response);
         return filmObj;
     })
-
-    .then(window.onload = filmObj => {
-
-        let main__form_list1 = document.body.querySelector('main__form-list1');
-        //main__form_list1.innerHTML = filmObj.Title;
-        //main__form_list1.appendChild(text, filmObj.Title);
-        console.log(main__form_list1.innerHTML);
+    .then(filmObj => {
+        let div = document.createElement('div');
+        div.className = 'film';
+        div.innerHTML = ` Title: <strong> ${filmObj.Title}</strong> <br> 
+        Year: ${filmObj.Year}<br>
+        Country: ${filmObj.Country}<br>
+        Genre: ${filmObj.Genre} <br>
+        Released: ${filmObj.Released} <br>
+        Plot: ${filmObj.Plot} <br>
+        <img class = "main main-img" src="https://ia.media-imdb.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg">
+        `;
+        document.body.appendChild(div);
 
     });
-/*
 
 
 
 
-let promise = new Promise((resolve, reject) => {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://www.omdbapi.com/?apikey=adac724d&t=batman", true);
-    //xhr.withCredentials = true;
-    xhr.onload = function Test() {
-        let film  = xhr.responseText;
-        let result =   document.getElementById("result");
-        result.innerHTML = film;
-    };
 
-    xhr.send();
-
-})
-
-promise.then(
-    result => onFulfilled, onRejected)
-
-let test = new Test();
-function test(){
-	forEach(function(index, film){
-    console.log (film.index);
-    });
-}
-
-
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("result").innerHTML = this.responseText;
-    }
-*/
