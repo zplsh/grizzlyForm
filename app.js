fd4fa14e17359a5e206054fc5eaf1ef5eccca737
pavel mikhailov <splash792@gmail.com>
2018-06-14 17:45:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-
 // Code goes here
-'use strict'
+'use strict';
 //Вешаем обработчик на форму
-document.getElementById('form1').addEventListener('submit', getInputVal)
-
-
+document.getElementById('form1').addEventListener('submit', getInputVal);
 
 function getInputVal(event) {
   let msg = document.getElementById('input1').value;
-
-  return modifyValToUrl(msg)
+  return modifyValToUrl(msg);
 }
-
-
-
 
 function httpGet(url, arrStr) {
   return new Promise(function(resolve, reject) {
     let xhr = new XMLHttpRequest();
     //alert (arrStr);
-    let urlStr = `${url}${arrStr}`
-
+    let urlStr = `${url}${arrStr}`;
 
     xhr.open('GET', urlStr, true);
     xhr.onload = function() {
@@ -30,11 +22,10 @@ function httpGet(url, arrStr) {
         let error = new Error(this.statusText);
         error.code = this.status;
         reject(error);
-        
       }
     };
     xhr.onerror = function() {
-      reject(new Error("Network Error"));
+      reject(new Error('Network Error'));
     };
     xhr.send();
   });
@@ -47,72 +38,85 @@ function modifyValToUrl(msg) {
   });
   var arrStr = arrExt.join('?');
 
-
-  httpGet('https://www.omdbapi.com/?apikey=adac724d&t=', `${arrStr}&plot=full
-`)
+  httpGet(
+    'https://www.omdbapi.com/?apikey=adac724d&t=',
+    `${arrStr}&plot=full
+`
+  )
     .then(response => {
       let filmObj = JSON.parse(response);
       return filmObj;
-
     })
     .then(filmObj => {
-      
+      let parent = document.getElementById('film-content');
       //ПРОВЕРКА - Если ошибка в вводе , то отобразить ошибку
       if (filmObj.Title === 'undefined' || filmObj.Title === undefined) {
-        
         let div = document.createElement('div');
-            div.setAttribute("id", "filmContent")
-            
-            div.innerHTML = `
+        div.setAttribute('id', 'filmContent');
+
+        div.innerHTML = `
       	    <div class="filmContent filmContent-error">
-      	    Некорректный ввод названия фильма! Х `
-            document.body.appendChild(div);
-            
-            let filmContentErr = document.getElementById('filmContent')
-            console.log(filmContentErr);
+      	    Некорректный ввод названия фильма! Х `;
+
+        parent.appendChild(div);
+
+        let filmContentErr = document.getElementById('filmContent');
+
         //Уничтожение окна с ошибкой по клику
-        filmContentErr.addEventListener('click', function(){
-            document.body.removeChild(filmContentErr)
-        })
-         //Уничтожение окна с ошибкой по таймеру
-        setTimeout(function(){ 
-            document.body.removeChild(filmContentErr);
+        filmContentErr.addEventListener('click', function() {
+          parent.removeChild(filmContentErr);
+        });
+        //Уничтожение окна с ошибкой по таймеру
+        setTimeout(function() {
+          parent.removeChild(filmContentErr);
         }, 2000);
-      } 
-       
-     
-      //Иначе добавить фильм на страницу.  
-     else {
+      }
+
+      //Иначе добавить фильм на страницу.
+      else {
         let div = document.createElement('div');
-        div.setAttribute("id", "filmContent")
-        
+
+        div.setAttribute('id', 'filmContent');
+
         div.innerHTML = `
       	<div class="film film__img" id ="innerFilm">
-          <img class = "main main-img" src = ${filmObj.Poster}> <br>
-          Title: <strong> ${filmObj.Title}</strong><br> 
-          Year: ${filmObj.Year}<br>
-          Country: ${filmObj.Country}<br>
-          Genre: ${filmObj.Genre}<br>
-          Released: ${filmObj.Released}<br>
-          Plot: ${filmObj.Plot}<br>
+      	<div class="container">
+      	  <div class="row">
+            <div class="col">
+            <img class = "main main-img" src = ${filmObj.Poster}> <br>
+            </div>
           </div>
-          </div>`;
+          <div class="row">
+            <div class="col">
+              Title: <strong> ${filmObj.Title} (${filmObj.Year})</strong><br> 
+              Director: <strong> ${filmObj.Director}</strong><br>
+              ImdbRating: <strong> ${filmObj.imdbRating}</strong><br>
+              Metascore: <strong> ${filmObj.Metascore}</strong><br>
+              Country: ${filmObj.Country}<br>
+              Genre: ${filmObj.Genre}<br>
+              Released: ${filmObj.Released}<br>
+              Plot: ${filmObj.Plot}<br>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
 
-        document.body.appendChild(div);
+        //Выводим найденный фильм
+        parent.appendChild(div);
+
+        //Рендерим фильм
         let filmContent = document.getElementById('filmContent');
-        return filmContent ;
+        if (filmContent) {
+          document
+            .getElementById('form1')
+            .addEventListener('submit', function(event) {
+              parent.removeChild(filmContent);
+            });
+        }
       }
-    })
-    .then(filmContent  => {
-      if (filmContent) {
-        document.getElementById('form1').addEventListener('submit', function(event) {
-          document.body.removeChild(filmContent);
-        });
-      }
-    })
-
-
+      return saveFilm(filmObj);
+    });
 }
-
 
 
